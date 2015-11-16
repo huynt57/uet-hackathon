44ef96b22ef89837f944fd87a0bbfdb4691e7c76
huynt <huynt57@gmail.com>
2015-11-16 11:33:26 +0700

    register

--- protected/models/User.php
+++ protected/models/User.php
@@ -2,14 +2,24 @@
 
 Yii::import('application.models._base.BaseUser');
 
-class User extends BaseUser
-{
-	public static function model($className=__CLASS__) {
-		return parent::model($className);
-	}
-        
-        public function register($attr)
-        {
-            
+class User extends BaseUser {
+
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
+    public function register($attr) {
+        $check = User::model()->findByAttributes(array('email' => $attr['email']));
+        if ($check) {
+            return 'USER_EXIST';
+        } else {
+            $model = new User;
+            $model->setAttributes($attr);
+            if ($model->save(FALSE)) {
+                return 'SUCCESS';
+            }
+            return 'SERVER_ERROR';
         }
+    }
+
 }
